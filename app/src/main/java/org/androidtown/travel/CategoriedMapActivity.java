04358e450ede91037d9e7a77cbbe0aad4002bd66
InjeongChoii <starec6230@kookmin.ac.kr>
2017-12-01 14:37:10 +0900

    Add Categorized map

---
+++ app/src/main/java/org/androidtown/travel/CategoriedMapActivity.java
@@ -0,0 +1,62 @@
+package org.androidtown.travel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+
+public class CategoriedMapActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageView headerImageView;
+
+    RadioButton redRadioBtn;
+    RadioButton yellowRadioBtn;
+    RadioButton greenRadioBtn;
+    RadioButton blueRadioBtn;
+    RadioButton blackRadioBtn;
+    RadioButton violetRadioBtn;
+    RadioButton totalRadioBtn;
+
+    ImageView mapImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categoried_map);
+
+        setView();
+    }
+
+    public void setView(){
+        headerImageView = (ImageView)findViewById(R.id.mapImageView);
+        totalRadioBtn = (RadioButton)findViewById(R.id.totalRadioBtn);
+        redRadioBtn = (RadioButton)findViewById(R.id.redRadioBtn);
+        yellowRadioBtn = (RadioButton)findViewById(R.id.yellowRadioBtn);
+        greenRadioBtn = (RadioButton)findViewById(R.id.greenRadioBtn);
+        blueRadioBtn = (RadioButton)findViewById(R.id.blueRadioBtn);
+        blackRadioBtn = (RadioButton)findViewById(R.id.blackRadioBtn);
+        violetRadioBtn = (RadioButton)findViewById(R.id.redRadioBtn);
+
+        mapImageView = (ImageView)findViewById(R.id.mapImageView);
+
+        totalRadioBtn.setOnClickListener(this);
+        redRadioBtn.setOnClickListener(this);
+        yellowRadioBtn.setOnClickListener(this);
+        greenRadioBtn.setOnClickListener(this);
+        blackRadioBtn.setOnClickListener(this);
+        blueRadioBtn.setOnClickListener(this);
+        violetRadioBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.redRadioBtn)    {mapImageView.setImageResource(R.drawable.map_red);}
+        else if(v.getId() == R.id.yellowRadioBtn)    {mapImageView.setImageResource(R.drawable.map_yellow);}
+        else if(v.getId() == R.id.greenRadioBtn)    {mapImageView.setImageResource(R.drawable.map_green);}
+        else if(v.getId() == R.id.blueRadioBtn)    {mapImageView.setImageResource(R.drawable.map_blue);}
+        else if(v.getId() == R.id.blackRadioBtn)    {mapImageView.setImageResource(R.drawable.map_black);}
+        else if(v.getId() == R.id.violetRadioBtn)    {mapImageView.setImageResource(R.drawable.map_violet);}
+        else if(v.getId() == R.id.totalRadioBtn)    {mapImageView.setImageResource(R.drawable.total_category_map);}
+    }
+}
