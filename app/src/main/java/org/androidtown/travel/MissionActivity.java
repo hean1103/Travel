cdc011b94f4a42ae2502b0f093b6195b9d9e9450
hean <hean1103@naver.com>
2017-12-03 20:47:01 +0900

    add TabActivity

---
+++ app/src/main/java/org/androidtown/travel/MissionActivity.java
@@ -0,0 +1,47 @@
+package org.androidtown.travel;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.SearchView;
+import android.view.View;
+import android.widget.ImageView;
+
+/**
+ * Created by hyean on 2017-12-02.
+ */
+
+public class MissionActivity extends ActionBarActivity{
+    int img1 = R.drawable.mission3;
+    int img2 = R.drawable.mission4;
+    ImageView iv;
+    ImageView iv2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mission);
+
+        iv = (ImageView)findViewById(R.id.image1);
+        iv.setImageResource(R.drawable.mission3b);
+
+        iv2 = (ImageView)findViewById(R.id.image2);
+        iv2.setImageResource(R.drawable.mission4b);
+
+        iv.setOnClickListener(new MyListner1());
+        iv2.setOnClickListener(new MyListner2());
+    }
+
+    class MyListner1 implements View.OnClickListener{
+        @Override
+        public void onClick(View v){
+            iv.setImageResource(img1);
+        }
+    }
+
+    class MyListner2 implements View.OnClickListener{
+        @Override
+        public void onClick(View v){
+            iv2.setImageResource(img2);
+        }
+    }
+}
